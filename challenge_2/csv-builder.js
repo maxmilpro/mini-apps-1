4a9416f95e6f64258239d7796d3592fae1a44e17
Max Proano <proanom89@gmail.com>
2021-01-27 20:26:28 -0800

    Setup csv-builder and imported to tests

---
+++ challenge_2/csv-builder.js
@@ -0,0 +1,12 @@
+module.exports = function() {
+  console.log('hello world');
+}
+
+/*
+ * Inputs - JSON object containing nested objects
+ * Outputs - csv file with formatted data
+ * Constraints -
+ * Edge Cases
+ *
+ *
+ */
--- challenge_2/test/test.js
+++ challenge_2/test/test.js
@@ -1,10 +1,12 @@
 var assert = require('assert');
 var chai = require('chai');
 var expect = chai.expect;
+var buildCSV = require('../csv-builder');
 
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
+      buildCSV();
       expect([1, 2, 3].indexOf(4)).to.equal(-1);
     });
   });
