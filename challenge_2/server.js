4164751a1dd077c81e34fb8aa73074a011c3e17b
Max Proano <proanom89@gmail.com>
2021-01-27 20:10:53 -0800

    Added express and get home route

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/', function(req, res) {
+  res.send('hello world');
+})
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+})
 
