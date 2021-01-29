ff990da9438132549edf40f2cee6da27de578726
Max Proano <proanom89@gmail.com>
2021-01-28 18:36:03 -0800

    Fixed issue with static file route

--- challenge_2/csv-builder.js
+++ challenge_2/csv-builder.js
@@ -2,11 +2,14 @@ module.exports = function() {
   console.log('hello world');
 }
 
-/*
+/**
  * Inputs - JSON object containing nested objects
  * Outputs - csv file with formatted data
  * Constraints -
  * Edge Cases
+ *
+ *
+ *
  *
  *
  */
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,11 +1,12 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('./client'));
+app.use('/', express.static(path.join(__dirname, 'client')));
 
-app.get('/', function(req, res) {
-  res.send('hello world');
+app.get('/file', function(req, res) {
+  res.send('getting the files for you');
 })
 
 app.listen(port, () => {
