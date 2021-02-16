ad2afb0fb88a6bb9b4be2c1908e36d65b20061bf
Max Proano <proanom89@gmail.com>
2021-02-16 15:42:05 -0800

    Connected to mongo and setup schema

---
+++ challenge_3/database/index.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+// connect to the db
+mongoose.connect('mongodb://localhost/purchases', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// test our connection
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function() {
+  console.log('We are connected!');
+});
+
+// define schema
+const purchaseSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  address: {
+    line1: String,
+    line2: String,
+    city: String,
+    state: String,
+    zipCode: String,
+    phoneNumber: String
+  },
+  billing: {
+    cc: String,
+    expiryDate: Date,
+    cvv: String,
+    zipCode: String
+  },
+  complete: Boolean
+});
+
+// compile schema into model
+const Purchase = mongoose.model('Purchase', purchaseSchema);
+
+const example = new Purchase();
+example.save();
+
+// insert record
+
+// update record
+
+
+
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -5,6 +5,31 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// POST /checkout
+app.post('/checkout', function(req, res) {
+
+});
+
+// POST /account
+app.post('/account', function(req, res) {
+
+});
+
+// POST /address
+app.post('/address', function(req, res) {
+
+});
+
+// POST /payment
+app.post('/payment', function(req, res) {
+
+});
+
+// POST /purchase
+app.post('/purchase', function(req, res) {
+
+});
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
