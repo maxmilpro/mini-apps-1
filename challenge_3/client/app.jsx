d086f883fc16a2aafadb48cc40c1b2c2acd18b2a
Max Proano <proanom89@gmail.com>
2021-02-16 15:14:09 -0800

    Setup components, state, and conditional rendering

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,8 +1,72 @@
 // const React = require('react');
 
+const Home = function(props) {
+  return (
+    <div>Checkout</div>
+  )
+}
+
+const CreateAccount = function(props) {
+  return (
+    <div>Create Account</div>
+  )
+}
+
+const ShippingAddress = function(props) {
+  return (
+    <div>Shipping Address</div>
+  )
+}
+
+const Payment = function(props) {
+  return (
+    <div>Payment</div>
+  )
+}
+
+const Summary = function(props) {
+  return (
+    <div>Summary</div>
+  )
+}
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recordId: null,
+      name: null,
+      email: null,
+      password: null,
+      address: {
+        line1: null,
+        line2: null,
+        city: null,
+        state: null,
+        zipCode: null,
+        phoneNumber: null
+      },
+      billing: {
+        cc: null,
+        expiryDate: null,
+        cvv: null,
+        zipCode: null
+      },
+      complete: false,
+    }
+  }
   render() {
-    return <h1>Hello World</h1>
+    if (this.state.recordId === null) {
+      return <Home/>
+    } else if (this.state.name === null) {
+      return <CreateAccount/>
+    } else if (this.state.address.line1 === null) {
+      return <ShippingAddress/>
+    } else if (this.state.billing.cc === null) {
+      return <Payment/>
+    } else {
+      return <Summary/>
+    }
   }
 }
 
