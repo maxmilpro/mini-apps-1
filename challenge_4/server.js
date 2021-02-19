cc35e5358d5c534a986ba937cdf2d51445b9e50c
Max Proano <proanom89@gmail.com>
2021-02-18 18:55:03 -0800

    Initialized npm and started express router

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,18 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+
+const port = 3000;
+
+// TO DO: add route to static
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err)
+  }
+  console.log(`Listening on https://localhost/${port}`)
+});
+
 
 
