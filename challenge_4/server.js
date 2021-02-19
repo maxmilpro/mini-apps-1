40d1af150b8b633445c7de764bf10ff37eee1377
Max Proano <proanom89@gmail.com>
2021-02-18 21:51:14 -0800

    Confirm we can serve static files from express

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,17 +1,20 @@
-var path = require('path');
-var express = require('express');
-var app = express();
+const path = require('path');
+const cors = require('cors');
+const express = require('express');
+const app = express();
 
 const port = 3000;
 
 // TO DO: add route to static
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
-app.listen(port, (err) => {
-  if (err) {
-    console.log(err)
-  }
-  console.log(`Listening on https://localhost/${port}`)
+app.get('/', (req, res) => {
+  res.send('test test test')
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`)
 });
 
 
